1863d1f5c6e9c22e0d6c75ed75775d781ff1b4de
snookca <snookca@users.noreply.github.com>
2023-06-17 19:36:21 +0000

    deploy: 26b4435544208b000ad880333b1dbde0772d37d5

--- .eleventy.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("img");
-  eleventyConfig.addPassthroughCopy("css");
-  eleventyConfig.addPassthroughCopy("files");
-};
