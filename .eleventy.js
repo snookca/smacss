59a5ede69db70726acd0ff2de3f278c224efa24c
Jonathan Snook <jonathan@snook.ca>
2023-06-17 15:58:48 -0400

    pass through favicon

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,5 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("files");
+  eleventyConfig.addPassthroughCopy("favicon.ico");
 };
